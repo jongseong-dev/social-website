e100137747f07a7a8ef1cd943a3aee9b86415819
jslee <dlwhdtjd098@gmail.com>
2024-05-05 22:23:26 +0900

    feat(account.urls): dashboard urls 추가

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
     # 직접 만든 로그인 뷰
@@ -7,4 +9,5 @@ urlpatterns = [
     # 장고에서 제공하는 인증 뷰
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", views.dashboard, name="dashboard"),
 ]
