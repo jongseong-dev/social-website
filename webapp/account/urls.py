896d3a8fedfab7ac203a30b3dfa78ee7199bba0e
jslee <dlwhdtjd098@gmail.com>
2024-05-05 21:44:13 +0900

    refactor(account.urls): 장고가 제공하는 인증 뷰로 교체

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
-from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    # 직접 만든 로그인 뷰
+    # path("login/", views.user_login, name="login"),
+    # 장고에서 제공하는 인증 뷰
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
