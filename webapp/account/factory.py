186ed4781d26288c27c354eeabdc54a9335d1c94
jslee <dlwhdtjd098@gmail.com>
2024-05-06 00:01:53 +0900

    test: 테스트 코드 추가

---
+++ webapp/account/factory.py
@@ -0,0 +1,11 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f"user{n}")
+    email = factory.LazyAttribute(lambda a: f"{a.username}@example.com")
+    password = factory.PostGenerationMethodCall("set_password", "password")
