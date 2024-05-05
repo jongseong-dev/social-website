86c2025254c5c05530f64ade046bc6f0326d840d
jslee <dlwhdtjd098@gmail.com>
2024-05-05 23:09:52 +0900

    feat(account.urls): 패스워드 재설정 urls 추가

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -20,5 +20,26 @@ urlpatterns = [
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
+    # 패스워드 재설정
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("", views.dashboard, name="dashboard"),
 ]
