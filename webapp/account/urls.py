5fa93540d2773d0e72bddc62cef954fb27b12482
jslee <dlwhdtjd098@gmail.com>
2024-05-05 22:41:11 +0900

    style(account.urls): 주석 변경

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -6,8 +6,19 @@ from . import views
 urlpatterns = [
     # 직접 만든 로그인 뷰
     # path("login/", views.user_login, name="login"),
-    # 장고에서 제공하는 인증 뷰
+    # 로그인 / 로그아웃
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # 패스워드 변경
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     path("", views.dashboard, name="dashboard"),
 ]
