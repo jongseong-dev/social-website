910042d8098f0a9792555498833fa17f4df6303d
jslee <dlwhdtjd098@gmail.com>
2024-05-06 22:08:19 +0900

    feat: 사용자 정보 수정 views 추가

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),  # 장고에서 제공하는 인증 URL include
     path("", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
 ]
