99c81b153fd2ac89a60ac354ab0cc0fe1d92e99d
jslee <dlwhdtjd098@gmail.com>
2024-05-06 21:18:40 +0900

    test: 사용자 등록 테스트 코드 추가

--- webapp/account/tests/test_forms.py
+++ webapp/account/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from webapp.account.forms import LoginForm
+from ..forms import LoginForm
+from ..forms import UserRegistrationForm
 
 
 class LoginFormTest(TestCase):
@@ -25,4 +26,40 @@ class LoginFormTest(TestCase):
     def test_form_is_invalid_with_long_username(self):
         self.form_data["username"] = "a" * 101
         form = LoginForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+
+class UserRegistrationFormTest(TestCase):
+    def setUp(self):
+        self.form_data = {
+            "username": "testuser",
+            "first_name": "testname",
+            "email": "test@test.com",
+            "password": "testpassword",
+            "password2": "testpassword",
+        }
+
+    def test_form_is_valid_with_correct_data(self):
+        form = UserRegistrationForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_is_invalid_with_empty_username(self):
+        self.form_data["username"] = ""
+        form = UserRegistrationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_form_is_invalid_with_empty_password(self):
+        self.form_data["password"] = ""
+        form = UserRegistrationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_form_is_invalid_with_mismatched_passwords(self):
+        self.form_data["password"] = "testpassword"
+        self.form_data["password2"] = "wrongpassword"
+        form = UserRegistrationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+    def form_is_invalid_with_invalid_email(self):
+        self.form_data["email"] = "invalidemail"
+        form = UserRegistrationForm(data=self.form_data)
         self.assertFalse(form.is_valid())
--- webapp/account/tests/test_views.py
+++ webapp/account/tests/test_views.py
@@ -28,3 +28,82 @@ class DashboardViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         expected_url = reverse("login") + "?next=" + quote_plus(self.url)
         self.assertRedirects(response, expected_url)
+
+
+class RegisterViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("register")
+        self.user = UserFactory.create(
+            username="testuser", email="test@test.com", password="testpassword"
+        )
+
+    def test_form_is_valid_with_correct_data(self):
+        response = self.client.post(
+            self.url,
+            {
+                "username": "newuser",
+                "email": "newuser@test.com",
+                "password": "newpassword",
+                "password2": "newpassword",
+            },
+        )
+        response.user = self.user
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/register_done.html")
+
+    def test_form_is_invalid_with_empty_username(self):
+        response = self.client.post(
+            self.url,
+            {
+                "username": "",
+                "email": "newuser@test.com",
+                "password": "newpassword",
+                "password2": "newpassword",
+            },
+        )
+        response.user = self.user
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/register.html")
+
+    def test_form_is_invalid_with_empty_password(self):
+        response = self.client.post(
+            self.url,
+            {
+                "username": "newuser",
+                "email": "newuser@test.com",
+                "password": "",
+                "password2": "newpassword",
+            },
+        )
+        response.user = self.user
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/register.html")
+
+    def test_form_is_invalid_with_mismatched_passwords(self):
+        response = self.client.post(
+            self.url,
+            {
+                "username": "newuser",
+                "email": "newuser@test.com",
+                "password": "newpassword",
+                "password2": "wrongpassword",
+            },
+        )
+        response.user = self.user
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/register.html")
+
+    def test_form_is_invalid_with_invalid_email(self):
+        response = self.client.post(
+            self.url,
+            {
+                "username": "newuser",
+                "email": "invalidemail",
+                "password": "newpassword",
+                "password2": "newpassword",
+            },
+        )
+        response.user = self.user
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/register.html")
