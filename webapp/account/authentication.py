50429a9ac0a8ee59022bfdc5088c279848109ae0
jslee <dlwhdtjd098@gmail.com>
2024-05-08 00:02:35 +0900

    modify: args 수정 username -> email

--- webapp/account/authentication.py
+++ webapp/account/authentication.py
@@ -6,9 +6,9 @@ class EmailAuthBackend:
     Email을 사용한 인증 백엔드
     """
 
-    def authenticate(self, request, username=None, password=None):
+    def authenticate(self, request, email=None, password=None):
         try:
-            user = User.objects.get(email=username)
+            user = User.objects.get(email=email)
             if user.check_password(password):
                 return user
             return None
