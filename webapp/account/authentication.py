58518fa31e3fda979293af5818515c870bd70d02
jslee <dlwhdtjd098@gmail.com>
2024-05-12 22:58:47 +0900

    feat: 소셜 인증을 통한 새로운 사용자 등록 시 Profile 생성

--- webapp/account/authentication.py
+++ webapp/account/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 
+from account.models import Profile
+
 
 class EmailAuthBackend:
     """
@@ -20,3 +22,16 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for social authentication
+
+    Args:
+    backend: 사용자 인증에 사용된ㄴ 소셜 인증 백엔드
+    user: user instance
+    args: additional arguments
+    kwargs: additional keyword arguments
+    """
+    Profile.objects.get_or_create(user=user)
