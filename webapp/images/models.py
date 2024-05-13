c9f353ab03def99ae1e26189fae7e6867f855cc1
jslee <dlwhdtjd098@gmail.com>
2024-05-13 21:02:07 +0900

    feat: image model 추가

--- webapp/images/models.py
+++ webapp/images/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="images_created",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=200, blank=True
+    )  # SEO 친화적인 URL을 만들기 위한 라벨
+    url = models.URLField()  # 이미지의 원래 URL
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=["-created"])]
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        제목 필드의 값을 기반으로 slug 필드를 자동으로 생성하도록 한다.
+        """
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
