c364f45e2481bc1a00b4152e53294b72b297b984
jslee <dlwhdtjd098@gmail.com>
2024-06-03 19:08:40 +0900

    feat: 사용자 활동스트림을 추적하기 위한 이벤트 함수 추가

--- webapp/account/views.py
+++ webapp/account/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 
+from actions.utils import create_action
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -21,6 +23,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(
                 request, "account/register_done.html", {"new_user": new_user}
             )
@@ -74,6 +77,7 @@ def user_detail(request, username):
     )
 
 
+@require_POST
 @login_required
 def user_follow(request):
     user_id = request.POST.get("id")
@@ -85,6 +89,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                     user_from=request.user, user_to=user
                 )
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(
                     user_from=request.user, user_to=user
