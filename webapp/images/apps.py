35c8c73d5313786e1326c20d0d9a3a191e169c89
jslee <dlwhdtjd098@gmail.com>
2024-06-05 21:09:48 +0900

    feat: signals가 작동하도록 apps에 ready 함수 추가

--- webapp/images/apps.py
+++ webapp/images/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "images"
+
+    def ready(self):  # 시그널을 등록하는 일반적인 방법
+        # signal 핸들러 임포트
+        import images.signals  # noqa
